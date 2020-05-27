4c35ec339602e61d5dcd7235aabf2f78a5eb6473
pupil <pupil@ivkhk.ee>
2020-05-27 11:12:03 +0300

    Работа за 27 мая

---
+++ src/components/indexComp.vue
@@ -0,0 +1,31 @@
+<template>
+    <v-container>
+        <v-row>
+            <v-col>
+                <p
+                v-for="(news,i) in news"
+                :key ="i"
+                >
+                {{news.text}}
+                </p>
+            </v-col>
+        </v-row>
+    </v-container>
+</template>
+<script>
+export default {
+    name: 'indexComp',
+    data:()=>({
+        news: [
+        {
+            text: 'Ffff',
+            href: ''
+        },
+        {
+            text: 'AAAA'
+        }
+        ],
+    }
+    )
+}
+</script>
--- src/views/index.vue
+++ src/views/index.vue
@@ -4,18 +4,21 @@
     <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div> -->
   <div>
-
+    <h1>Добро пожаловать на сайт Anime List</h1>
+    <indexComp msg="Test"/>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 // import HelloWorld from '@/components/HelloWorld.vue'
+import indexComp from '@/components/indexComp.vue'
 
 export default {
   name: 'Index',
   components: {
     // HelloWorld
+    indexComp
   }
 }
 </script>
